ff6b917aa2185e20eb49e5ead22dbf7b894e131e
Aanya-009 <aanyag009@gmail.com>
2022-05-07 14:19:41 +0545

    Added messages and like button

--- js/kwitter_page.js
+++ js/kwitter_page.js
@@ -22,6 +22,18 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
+         console.log(firebase_message_id);
+         console.log(message_data);
+         name = message_data['name'];
+         likes = message_data['likes'];
+         message = message_data['message'];
+         name_with_tag = "<h4>"+name+"<img class='user_tick' src='../img/tick.png'></h4>";
+         message_with_tag = "<h4 class='message_h4'>"+message+"</h4>";
+         like_button_with_tag = "<button class='btn btn-warning' id="+firebase_message_id+" value="+likes+" onclick='update_likes(this.id)'>";
+         button_span_tag = "<span class='glyphicon glyphicon-thumbs-up'> Likes: "+likes+"</span></button><hr>";
+         row = name_with_tag+ message_with_tag+like_button_with_tag+ button_span_tag;
+         document.getElementById("output").innerHTML += row;
+
 //End code
       } });  }); }
 getData();
@@ -40,4 +52,16 @@ function send(){
             likes : 0
       });
       document.getElementById("msg").innerHTML = "";
+}
+
+function update_likes(message_id){
+      console.log("Clicked on a like button"+message_id);
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
+      updated_likes = Number(likes)+1;
+      console.log(updated_likes);
+
+      firebase.database().ref(room_name).child(message_id).update({
+            likes : updated_likes
+      });
 }
